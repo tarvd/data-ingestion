798b45b9d0e85a70099ae5b042f06564d2aa5910
Travis Douglas <tedward27@gmail.com>
2024-11-03 11:20:57 -0600

    Add S3 upload

--- data_ingestion/data_ingestion.py
+++ data_ingestion/data_ingestion.py
@@ -1,5 +1,5 @@
 import os 
-import zipfile
+from zipfile import ZipFile
 import requests 
 import awswrangler as wr 
 
@@ -10,7 +10,7 @@ def download_file_from_url(
 ) -> None:
     response = requests.get(url)
     print(f"Data read from: {url}")
-    local_filepath = os.path.join("local_dir", "local_filename")
+    local_filepath = os.path.join(local_dir, local_filename)
     try:
         with open(local_filepath, 'wb') as file:
             file.write(response.content)
@@ -19,31 +19,34 @@ def download_file_from_url(
         print(f"An error occurred when downloading the file from url: {e}")
 
 def extract_data_from_archive(
-    zip_file: zipfiletype, 
+    zip_file: ZipFile, 
     extract_dir: str, 
     extension: str = ".csv"
-) -> ospathjoinfiletype:
-    archive_data_filepath = [x for x in zip_file.namelist() if os.path.splitext(x)[-1] = ".csv"][0]
+) -> str:
+    archive_data_filepath = [x for x in zip_file.namelist() if os.path.splitext(x)[-1] == ".csv"][0]
     archive_data_filename = os.path.basename(archive_data_filepath)
     print(f"Data found at: {archive_data_filepath}")
     extracted_data_filepath = os.path.join(
         extract_dir,
         archive_data_filename
     )
-    try:
-        with open(extracted_data_filepath, "wb") as f:
-            f.write(zip_file.read(archive_data_filepath))
-        print(f"Data extracted to: {extracted_data_filepath}")
-    except Exception as e:
-        print(f"An error occurred when extracting the data from the archive: {e}")
+    if os.path.isfile(extracted_data_filepath):
+        print(f"Data already exists at: {extracted_data_filepath}")
+    else:
+        try:
+            with open(extracted_data_filepath, "wb") as f:
+                f.write(zip_file.read(archive_data_filepath))
+            print(f"Data extracted to: {extracted_data_filepath}")
+        except Exception as e:
+            print(f"An error occurred when extracting the data from the archive: {e}")
     return extracted_data_filepath
 
-# def upload_file_to_s3(local_filename, bucket_name, s3_filename):
-#     s3_path = f"s3://{bucket_name}/{s3_filename}"
-#     try:
-#         wr.s3.upload(local_filename, s3_path)
-#     except Exception as e:
-#         print(f"An error occurred when uploading the file to S3: {e}")
+def upload_file_to_s3(local_filepath, bucket_name, s3_filename):
+    s3_path = f"s3://{bucket_name}/{s3_filename}"
+    try:
+        wr.s3.upload(local_filepath, s3_path)
+    except Exception as e:
+        print(f"An error occurred when uploading the file to S3: {e}")
 
 def main():
     # Download file from url
@@ -54,11 +57,11 @@ def main():
         "temp"
     )
     archive_filename = "openpowerlifting-latest.zip"
-    download_file_from_url(url, local_filename)
+    # download_file_from_url(url, archive_filename, archive_dir)
 
     # Extract data from archive
-    zip_file = zipfile.ZipFile(archive_filename, "r")
-    print(type(zip_file))
+    archive_filepath = os.path.join(archive_dir, archive_filename)
+    zip_file = ZipFile(archive_filepath, "r")   
     extract_dir = os.path.join(
         ".",
         "data",
@@ -68,10 +71,11 @@ def main():
     )
     extracted_data_filepath = extract_data_from_archive(zip_file, extract_dir)
     
-    # bucket_name = "tdouglas-data-prod-useast2"
-    # s3_filename = ""
-    # data_filename = extract_data_from_archive(archive_filename, extract_dir)
-    # print(data_filename)
+    # Upload the file to S3
+    bucket_name = "tdouglas-data-prod-useast2"
+    s3_filename = f"data/raw/openpowerlifting/obt/csv/{os.path.basename(extracted_data_filepath)}"
+    upload_file_to_s3(extracted_data_filepath, bucket_name, s3_filename)
 
 if __name__ == "__main__":
     main()
+    # python data_ingestion.py
