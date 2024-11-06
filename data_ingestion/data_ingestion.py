e7e750e9abfcb0bdfe40831b6bbdf494f873a94d
Travis Douglas <tedward27@gmail.com>
2024-11-05 21:30:08 -0600

    update functions

--- data_ingestion/data_ingestion.py
+++ data_ingestion/data_ingestion.py
@@ -19,83 +19,74 @@ def download_data_opl_lifter(
     except Exception as e:
         print(f"An error occurred when connecting to the website: {e}")
 
-    try:
-        # Save the response as an archive
-        with open(zip_filename, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:
-                    file.write(chunk)
-        print(f"Archive saved to: {zip_filename}")
-    except Exception as e:
-        print(f"An error occurred when downloading the archive file: {e}")
-
+    # Save the response as an archive
+    with open(zip_filename, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                file.write(chunk)
+    print(f"Archive saved to: {zip_filename}")
+    
 
 def extract_data_opl_lifter(
     zip_filename: str
 ) -> str:
-    try:
-        # Extract the data file from the archive file
-        with ZipFile(zip_filename, "r") as z:
-            data_filename_in_archive = [
-                x for x in z.namelist() if os.path.splitext(x)[-1] == ".csv"
-            ][0]
-            print(f"Data found at: $archive/{data_filename_in_archive}")
-            data_basename = os.path.basename(data_filename_in_archive).replace(
-                "openpowerlifting", "lifter"
-            )
-            data_temp_filename = os.path.join(os.getcwd(), "data", "temp", data_basename)
+    with ZipFile(zip_filename, "r") as z:
+        # Find the data file in the archive
+        data_filename_in_archive = [
+            x for x in z.namelist() if os.path.splitext(x)[-1] == ".csv"
+        ][0]
+        print(f"Data found at: $archive/{data_filename_in_archive}")
+        data_basename = os.path.basename(data_filename_in_archive).replace(
+            "openpowerlifting", "lifter"
+        )
+        data_temp_filename = os.path.join(os.getcwd(), "data", "temp", data_basename)
+        
+        # Extract the data file
+        with z.open(data_filename_in_archive, "r") as csv_in_zip:
             with open(data_temp_filename, "wb") as f:
-                f.write(z.read(data_filename_in_archive))
-            print(f"Data extracted to: {data_temp_filename}")
-            return data_temp_filename
-    except Exception as e:
-        print(
-            f"An error occurred when extracting the data file from the archive file: {e}"
-        )
+                while True:
+                    chunk = csv_in_zip.read(8192)
+                    if not chunk:
+                        break 
+                    f.write(chunk)
+    print(f"Data extracted to: {data_temp_filename}")
+    return data_temp_filename
 
 
-def parse_data_file_to_csv(
-    data_temp_filename: str, download_timestamp: pd.Timestamp = None
+def add_metadata_columns(
+    filename, 
+    download_timestamp: pd.Timestamp = None
+):
+    # Add metadata columns
+    output_filename = filename.replace(".csv","_modified.csv")
+    chunk_iter = pd.read_csv(filename, dtype=str, chunksize=300000)
+    for chunk in chunk_iter:
+        chunk["downloaded_at"] = download_timestamp
+        chunk["created_date"] = download_timestamp.strftime("%Y%m%d")
+        chunk.to_csv(output_filename, mode="a", index=False)
+
+    # Replace the old file with the new one
+    os.remove(filename)
+    os.rename(output_filename, filename)
+    print(f"Modified data in: {filename}")
+    print(f"Column added to the data: downloaded_at")
+    print(f"Column added to the data: created_date")
+
+
+def convert_csv_to_parquet(
+    data_temp_filename: str
 ) -> str:
-    try:
-        # Add metadata columns and save to csv
-        df = pd.read_csv(data_temp_filename, dtype=str)
-        df["downloaded_at"] = download_timestamp
-        print(f"Column added to the data: downloaded_at")
-        df["created_date"] = download_timestamp.strftime("%Y%m%d")
-        print(f"Column added to the data: created_date")
-        csv_basename = os.path.basename(data_temp_filename)
-        csv_filename = os.path.join(
-            os.getcwd(), "data", "raw", "openpowerlifting", "lifter", csv_basename
-        )
-        df.to_csv(csv_filename, index=False)
-        print(f"Data parsed and saved to: {csv_filename}")
-        return csv_filename
-    except Exception as e:
-        print(f"An error occurred when parsing the data file: {e}")
-
-
-def parse_data_file_to_parquet(
-    data_temp_filename: str, download_timestamp: pd.Timestamp = None
-) -> str:
-    try:
-        # Add metadata columns and save to parquet
-        df = pd.read_csv(data_temp_filename, dtype=str)
-        df["downloaded_at"] = download_timestamp
-        print(f"Column added to the data: downloaded_at")
-        df["created_date"] = download_timestamp.strftime("%Y%m%d")
-        print(f"Column added to the data: created_date")
-        parquet_basename = (
-            f"{os.path.splitext(os.path.basename(data_temp_filename))[0]}.parquet"
-        )
-        parquet_filename = os.path.join(
-            os.getcwd(), "data", "raw", "openpowerlifting", "lifter", parquet_basename
-        )
-        df.to_parquet(parquet_filename, index=False)
-        print(f"Data parsed and saved to: {parquet_filename}")
-        return parquet_filename
-    except Exception as e:
-        print(f"An error occurred when parsing the data file: {e}")
+    # Convert to parquet format
+    df = pd.read_csv(data_temp_filename, dtype=str)
+    parquet_basename = (
+        f"{os.path.splitext(os.path.basename(data_temp_filename))[0]}.parquet"
+    )
+    parquet_filename = os.path.join(
+        os.getcwd(), "data", "raw", "openpowerlifting", "lifter", parquet_basename
+    )
+    df.to_parquet(parquet_filename, index=False)
+    print(f"Data parsed and saved to: {parquet_filename}")
+    return parquet_filename
 
 
 def upload_file_to_s3(
@@ -126,12 +117,12 @@ def main():
         os.getcwd(), "data", "temp", "openpowerlifting-latest.zip"
     )
     download_data_opl_lifter(zip_filename)
-    download_timestamp = pd.Timestamp.utcnow()
     data_temp_filename = extract_data_opl_lifter(zip_filename)
-    parquet_filename = parse_data_file_to_parquet(data_temp_filename, download_timestamp)
-    upload_file_to_s3(parquet_filename)
+    download_timestamp = pd.Timestamp.utcnow()
+    add_metadata_columns(data_temp_filename, download_timestamp)
+    parquet_filename = convert_csv_to_parquet(data_temp_filename)
+    # upload_file_to_s3(parquet_filename)
 
 
 if __name__ == "__main__":
     main()
-    # python data_ingestion.py
