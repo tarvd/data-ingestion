b5311285affd4135093e9ac32da6d09aff554096
Travis Douglas <tedward27@gmail.com>
2024-11-06 01:50:04 -0600

    Save before cutting down code attempt

--- data_ingestion/data_ingestion.py
+++ data_ingestion/data_ingestion.py
@@ -1,17 +1,18 @@
 import os
+import sys
 import hashlib
-import json
 import logging
 from zipfile import ZipFile
 
 import requests
 import pandas as pd
 import awswrangler as wr
+import pyarrow as pa
 
 
 def download_zip_opl_lifter(
-    zip_file: str,
-    url: str = "https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip",
+    url: str,
+    zip_file: str 
 ) -> None:
     try:
         # Get a response from the url
@@ -28,18 +29,17 @@ def download_zip_opl_lifter(
     print(f"Archive saved to: {zip_file}")
 
 
-def extract_data_opl_lifter(zip_file: str, ):
+def extract_data_opl_lifter(
+    zip_file: str,
+    data_file: str
+) -> None:
     with ZipFile(zip_file, "r") as z:
         # Find the data file in the archive
         data_archive_file = [
             x for x in z.namelist() if os.path.splitext(x)[-1] == ".csv"
         ][0]
         print(f"Data found at: $archive/{data_archive_file}")
-        data_basename = os.path.basename(data_archive_file).replace(
-            "openpowerlifting", "lifter"
-        )
-        data_file = os.path.join(os.getcwd(), "data", "temp", data_basename)
-
+        
         # Extract the data file
         with z.open(data_archive_file, "r") as csv_in_zip:
             with open(data_file, "wb") as f:
@@ -51,7 +51,9 @@ def extract_data_opl_lifter(zip_file: str, ):
     print(f"Data extracted to: {data_file}")
 
 
-def add_metadata_columns(data_file: str, download_timestamp: pd.Timestamp = None) -> None:
+def add_metadata_columns(
+    data_file: str, download_timestamp: pd.Timestamp = None
+) -> None:
     # Add metadata columns
     output_file = data_file.replace(".csv", "_modified.csv")
     chunk_iter = pd.read_csv(data_file, dtype=str, chunksize=300000)
@@ -68,16 +70,15 @@ def add_metadata_columns(data_file: str, download_timestamp: pd.Timestamp = None
     print(f"Column added to the data: created_date")
 
 
-def convert_csv_to_parquet(data_file: str, parquet_file: str) -> None:
+def convert_csv_to_parquet_old(data_file: str, parquet_file: str) -> None:
     # Convert to parquet format
-    parquet_basename = (
-        f"{os.path.splitext(os.path.basename(data_file))[0]}.parquet"
-    )
-    parquet_file = os.path.join(
-        os.getcwd(), "data", "temp", parquet_basename
-    )
     df = pd.read_csv(data_file, dtype=str)
     df.to_parquet(parquet_file, index=False)
+    print(f"Data converted and saved to: {parquet_file}")
+
+def convert_csv_to_parquet(data_file: str, parquet_file: str) -> None:
+    # Convert to parquet format
+    pa
     print(f"Data converted and saved to: {parquet_file}")
 
 
@@ -88,35 +89,40 @@ def get_checksum(file: str) -> str:
             hash_func.update(chunk)
     return hash_func.hexdigest()
 
+
 def main():
     log_file = "logfile.log"
     logging.basicConfig(
-                    filename=log_file,
-                    level=logging.INFO,
-                    format='%(asctime)s - %(message)s', 
-                    datefmt='%Y-%m-%d %H:%M:%S') 
-    
-    zip_file = os.path.join(
-        os.getcwd(), "data", "temp", "openpowerlifting-latest.zip"
+        filename=log_file,
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
-    download_zip_opl_lifter(zip_file)
-    checksum = get_checksum(zip_file)[:32]    
-    data_file = os.path.join(
-        os.getcwd(), "data", "temp", f"lifter-{pd.Timestamp.utcnow().strftime("%Y%m%d")}-{checksum}.csv"
-    )
+
+    url = "https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip"
+    zip_file = os.path.join(os.getcwd(), "data", "temp", "openpowerlifting-latest.zip")
+    logging.info(f"Program started with url={url} and zip_file={zip_file}")
+    download_zip_opl_lifter(url, zip_file)
+    checksum = get_checksum(zip_file)[:32]
+    data_file = os.path.join(os.getcwd(), "data", "temp", f"lifter-{checksum}.csv")
     parquet_file = data_file.replace(".csv", ".parquet")
     s3_path = f"s3://tdouglas-data-prod-useast2/data/raw/openpowerlifting/lifter/{os.path.basename(parquet_file)}"
-    logging.info(f"{zip_file}, {checksum}")
+    
+    logging.info(f"s3_path={s3_path}, checksum={checksum}")
 
-    if wr.s3.does_object_exist(s3_path):
+    # if wr.s3.does_object_exist(s3_path):
+    if 1 == 2:
         print(f"File already exists on S3: {s3_path}")
+        logging.info(f"Program ended after finding an S3 object at {s3_path}")
+        sys.exit()
     else:
-        extract_data_opl_lifter(zip_file)
+        extract_data_opl_lifter(zip_file, data_file)
         add_metadata_columns(data_file, download_timestamp=pd.Timestamp.utcnow())
         convert_csv_to_parquet(data_file, parquet_file)
-        wr.s3.upload(parquet_file, s3_path)
-        print(f"Data uploaded to: {s3_path}")
-        
+        # wr.s3.upload(parquet_file, s3_path)
+        # print(f"Data uploaded to: {s3_path}")
+        # logging.info(f"S3 object uploaded at {s3_path}")
+
 
 if __name__ == "__main__":
     main()
--- data_ingestion/steps.md
+++ data_ingestion/steps.md
@@ -1,12 +1,14 @@
 1. Run data_ingestion.py to get new parquet file in S3.
 2. Run Glue crawler to update schema if necessary.
 3. Athena now has all history in lifter table.
-4. An Iceberg table has been created at 
-4. v_lifter now pulls only the last day from the lifter table.
+4. An Iceberg table has been created in openpowerlifting schema 
+4. v_lifter pulls only the last day from the lifter table.
 5. v_lifter_iceberg_delta pulls only the days after the latest iceberg ingestion.
 6. insert_into_lifter_iceberg will insert content from v_lifter_iceberg_delta into lifter_iceberg
 
 1. Run data_ingestion.py every day checking if file is already downloaded.
+    Blocker: The hash method of checking data integrity does not seem to work. 
+    Solution: Consider just setting up a weekly download with a check to see if data updated or not once in Athena.
 2. Files should be added weekly.
 3. Run Glue crawler when file is added to S3 bucket.
 4. Import data into lifter_iceberg also.
